00c7aa43b4abe9c85f56766dd33999ca665b3baf
Daniel Zettel <daniel.zettel@lv.campus02.at>
2026-03-18 19:04:35 +0100

    bufferedreader and printwriter implementation

--- src/org/campus02/urldemo/MyFirstUrl.java
+++ src/org/campus02/urldemo/MyFirstUrl.java
@@ -1,12 +1,8 @@
 package org.campus02.urldemo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class MyFirstUrl {
 
@@ -20,12 +16,27 @@ public class MyFirstUrl {
 
             // zum lesen verwenden wir den Inputstream meiner "myUrl"
             try (BufferedReader br = new BufferedReader(
-                    new InputStreamReader(myUrl.openStream()))) {
-
+                    new InputStreamReader(myUrl.openStream()));
+                 BufferedWriter bw = new BufferedWriter(
+                         new FileWriter("data/campus02.html")
+                 );
+                 PrintWriter pw = new PrintWriter(
+                         new FileWriter("data/campus02_pw.html")
+                 )
+            ) {
                 String html;
                 while ((html = br.readLine()) != null) {
                     System.out.println(html);
+
+                    // schreibe daten in file
+                    bw.write(html);
+                    // zeilenumbruch
+                    bw.newLine();
+
+                    pw.println(html);
                 }
+                bw.flush();
+                pw.flush();
 
             }
         } catch (MalformedURLException e) {
